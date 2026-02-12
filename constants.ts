a699edd38310655046c286243f196c69ddf0c107
Sabaris <65608480+sabaris-sps@users.noreply.github.com>
2026-02-12 21:16:53 +0530

    Add module_2.json to constants

--- constants.ts
+++ constants.ts
@@ -1,10 +1,13 @@
 import { TestData, TestMetadata } from './types';
-import module_1 from './tests/module_1.json'
+import module_1 from './tests/module_1.json';
+import module_2 from './tests/module_2.json';
 
 export const TESTS: TestMetadata[] = [
   { id: '1', name: 'Module 1', fileName: 'module_1.json' },
+  { id: '2', name: 'Module 2', fileName: 'module_2.json' },
 ];
 
 export const MOCK_TEST_DATA: Record<string, TestData> = {
-  'module_1.json': module_1 as TestData
+  'module_1.json': module_1 as TestData,
+  'module_2.json': module_2 as TestData
 };
