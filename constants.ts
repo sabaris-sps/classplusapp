6faec7b8aeea6d33cc116122f115f4782ada6745
sabaris-sps <spssabaris@gmail.com>
2026-02-14 21:55:24 +0530

    Added module 3

--- constants.ts
+++ constants.ts
@@ -1,13 +1,16 @@
 import { TestData, TestMetadata } from './types';
 import module_1 from './tests/module_1.json';
 import module_2 from './tests/module_2.json';
+import module_3 from './tests/module_3.json';
 
 export const TESTS: TestMetadata[] = [
   { id: '1', name: 'Module 1', fileName: 'module_1.json' },
   { id: '2', name: 'Module 2', fileName: 'module_2.json' },
+  { id: '3', name: 'Module 3', fileName: 'module_3.json' },
 ];
 
 export const MOCK_TEST_DATA: Record<string, TestData> = {
   'module_1.json': module_1 as TestData,
-  'module_2.json': module_2 as TestData
+  'module_2.json': module_2 as TestData,
+  'module_3.json': module_3 as TestData
 };
